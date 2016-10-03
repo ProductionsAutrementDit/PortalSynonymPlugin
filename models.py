b40f8b71d01dc486828583ddcd564a1a95846749
Camille Darley <camdarley@gmail.com>
2016-10-03 09:23:23 +0200

    Add ordering on synonym model

--- models.py
+++ models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Synonym(models.Model):
     value = models.CharField(max_length=255)
     parent = models.ForeignKey("self", related_name="synonyms", max_length=100, on_delete=models.CASCADE, blank=True, null=True)
+    
+    class Meta:
+        ordering = ['value']
 
 class TagField(models.Model):
     fieldname = models.CharField(max_length=255)
